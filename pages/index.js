493cfc148089b3a4bd0cda6772f8f19b4b00e9b0
Ming-der Wnag <mingderwang@gmail.com>
2021-12-28 22:23:54 +0800

    add daisyUI demo button

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,9 @@ export default function Index() {
   console.log(data)
   return (
     <div>
+      <button class="inline-block px-4 py-3 text-sm font-semibold text-center text-white uppercase transition duration-200 ease-in-out bg-indigo-500 rounded-md cursor-pointer hover:bg-indigo-600">Tailwind Button</button>
+      <button class="btn btn-primary">daisyUI Button</button>
+
       {users.map((user, i) => (
         <h1 className="text-3xl font-bold underline"  key={i}>{user.id}</h1>
       ))}
