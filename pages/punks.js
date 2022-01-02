d286952d7ab53ee508c3a9a240fc67b5c6da24de
Ming-der Wnag <mingderwang@gmail.com>
2022-01-02 16:23:41 +0800

    display mingder78s pins

--- pages/punks.js
+++ pages/punks.js
@@ -4,7 +4,7 @@ import useSWR from 'swr'
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 export default function Index() {
-  const { data, error } = useSWR('/api/pins', fetcher)
+  const { data, error } = useSWR('/api/punks', fetcher)
 
   if (error) return <div>Failed to load</div>
   if (!data) return <div>Loading...</div>
@@ -12,7 +12,6 @@ export default function Index() {
   const punks = data.slice(0,21) // only show first 20 punks
 
   const jsony = (a) => {
-    console.log('a', a)
     return Object.entries(a)
       .map(([k, v]) => `${k}: ${v}`) // stringfy an json object a
       .join(`,\n `)
@@ -24,11 +23,11 @@ export default function Index() {
         <div className="flex flex-nowrap">
           <div className="card w-72 card-bordered card-compact lg:card-normal">
             <figure>
-              <img src={`${punk.images['237x'].url}`}></img>
+              <img src={`${punk.image}`}></img>
             </figure>
             <div className="card-body">
-              <h2 className="card-title">{punk.pinner.full_name}</h2>
-              <p>{punk.description}</p>
+              <h2 className="card-title">{(punk.type === 'Male')? '': '' }</h2>
+              <pre>{jsony(punk.accessories)}</pre>
             </div>
           </div>
         </div>
